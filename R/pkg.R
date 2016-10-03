4bf967465d8e5106950b3e51e819bf51c59ed3e3
Emaasit <daniel.emaasit@gmail.com>
2016-10-03 13:55:05 -0700

    3rd commit

--- R/pkg.R
+++ R/pkg.R
@@ -1,7 +1,7 @@
-#' @title Read in SAS datasets (.sas7bdat files) into Spark
-#' @description 'spark.sas7bdat' uses the spark-sas7bdat Spark package to load SAS dataset in parallel in Spark.
-#' @name spark.sas7bdat-package
-#' @aliases spark.sas7bdat-package
+#' @title Read in different data files into Spark
+#' @description 'sparkreadr' uses several Spark packages to load datasets in parallel in Spark.
+#' @name sparkreadr
+#' @aliases sparkreadr-package
 #' @docType package
 #' @importFrom sparklyr hive_context invoke spark_dependency register_extension sdf_register
 #' @importFrom magrittr "%>%"
